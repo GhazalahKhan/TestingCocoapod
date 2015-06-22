2adec78cefb32e337bf5768a3f0618bdd8979959
GhazalahKhan <ghazalah.khan@kayosys.com>
2015-06-22 12:12:02 +0530

    spec created ...2 classes added

---
+++ Pod/Classes/ViewController1.h
@@ -0,0 +1,13 @@
+//
+//  ViewController1.h
+//  Pods
+//
+//  Created by Kayosys on 6/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController1 : UIViewController
+
+@end
---
+++ Pod/Classes/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  Pods
+//
+//  Created by Kayosys on 6/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
